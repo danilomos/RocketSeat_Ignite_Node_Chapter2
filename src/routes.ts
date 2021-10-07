af662783be645097be8f066ffe277523e7f58846
Danilo Moreira da Silva <xliudanilox@gmail.com>
2021-10-07 15:39:50 -0300

    feat: create category

--- src/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-import CreateCourseService from "./CreateCourseService";
-
-export function createCouse(req: Request, res: Response) {
-
-    CreateCourseService.execute({ name: "NodeJS", duration: 10, educator: "Danilo" });
-
-    return res.send()
-}
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,20 @@
-import express from "express";
+import express from 'express';
 
-import { createCouse } from "./routes";
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
-app.get("/", createCouse);
+app.use(express.json());
 
-app.listen(3333);
+app.use('/categories', categoriesRoutes);
+
+app.get('/', (request, response) => {
+    return response.json({ messsage: 'Hello World' });
+});
+
+app.post('/courses', (request, response) => {
+    const { name } = request.body;
+    return response.json({ name });
+});
+
+app.listen(3333, () => console.log('Server is running.'));
